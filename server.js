8314c6f8b4e38711d1acb56ace654127da66dcad
= <coreyauger@gmail.com>
2015-12-27 21:21:24 -0800

    added ssl support:

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(8888);
+var https = require('https');
+var fs = require('fs');
+
+//connect().use(serveStatic(__dirname)).listen(9999);
+var options = {
+    key:    fs.readFileSync('ssl/server.key'),
+    cert:   fs.readFileSync('ssl/server.crt')//,
+//    ca:     fs.readFileSync('ssl/ca.crt')
+};
+var app = connect().use(serveStatic(__dirname));
+https.createServer(options,app).listen(9999);
+
